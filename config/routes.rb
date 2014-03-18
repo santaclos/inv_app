cf60f79d37af5c1b37619cdf05d9b7a0dc6f6806
Rafa <rafaelhealy@gmail.com>
2014-03-17 23:04:07 -0600

    fin de chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 InvApp::Application.routes.draw do
+  get "users/new"
+  
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
 end
